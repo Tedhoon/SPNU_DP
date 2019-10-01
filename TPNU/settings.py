3fdee5bca2d0830b4c81242f6f52d304b0d98c8d
Tedhoon <gt0305@likelion.org>
2019-10-01 23:48:02 +0900

    dp

--- TPNU/settings.py
+++ TPNU/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9wa$evh02zs$lx@#kx!x-=90zq^+d2td(o_-sn=lw#uea53l(j'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,12 +80,27 @@ WSGI_APPLICATION = 'TPNU.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# Postgresql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'TPNU',
+#         'USER': 'postgres',
+#         'PASSWORD': '16331541z',
+#         'HOST' : 'localhost',
+#         'PORT' : '5432',
+
+#     }
+# }
 
 
 # Password validation
