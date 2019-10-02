b05865ee548870733354cc2c744a0f41d7df76c0
Tedhoon <gt0305@likelion.org>
2019-10-03 06:02:30 +0900

    dp update

--- imageboard/urls.py
+++ imageboard/urls.py
@@ -11,9 +11,6 @@ urlpatterns = [
     path('image/post/', ImagePost.as_view() , name = 'image_post'),
     path('image/edit/<int:image_detail_id>/' , image_edit , name = 'image_edit'),
     path('image/delete/<slug:pk>/', ImageDelete.as_view() , name = 'image_delete'),
-    # path('image/like/<int:image_id>/', post_like, name='post_like'),
-    # path('image/like/<slug:pk>/', PostLikeRedirect.as_view() , name = 'like'),
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
