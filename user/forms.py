aead15346040d262ae1dcddcb67b7c21bad1e5d6
Tedhoon <gt0305@likelion.org>
2019-10-07 00:35:24 +0900

    dp

--- user/forms.py
+++ user/forms.py
@@ -18,7 +18,7 @@ class RegisterForm(forms.ModelForm):
         email = self.cleaned_data.get('email', '')
         if email :
             if get_user_model().objects.filter(email=email).exists():
-                raise forms.ValidationError('이미 사용중인 이메일입니다!!!')
+                raise forms.ValidationError('이미 사용중인 이메일 또는 인증되지 않은 이메일입니다.')
         return email
         # 중복 이메일 못받게 하기
 
@@ -29,5 +29,13 @@ class RegisterForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords not matched!')
+            raise forms.ValidationError('두 비밀번호가 다릅니다.')
         return cd['password2']
+
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username', '')
+        if username :
+            if get_user_model().objects.filter(username=username).exists():
+                raise forms.ValidationError('해당 ID가 존재하거나 인증되지 않은 유저입니다. 가입시 입력한 이메일을 확인하세요.')
+        return username
